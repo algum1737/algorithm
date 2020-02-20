49ff037a1f0eb5b6eb29274a3c718ecc0fd35cd7
백단향 <@DESKTOP-O1G69RV>
2020-02-20 19:00:41 +0900

    git 열심히 써봐야된다.

---
+++ algoex/src/day02/coddup1021.java
@@ -0,0 +1,12 @@
+package day02;
+
+import java.util.Scanner;
+
+public class coddup1021 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String c = sc.nextLine();
+		System.out.println(c);
+	}
+}
